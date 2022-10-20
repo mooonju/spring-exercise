e3b18c56939873926a24d5d2caf861f151ff52a2
MUNJU <mvv0030@gmail.com>
2022-10-20 14:35:30 +0900

    Spring 복습 before each 적용

--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -1,10 +1,12 @@
 package com.likelion.dao;
 
 import com.likelion.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -19,18 +21,30 @@ class UserDaoTest {
     @Autowired
     ApplicationContext context;
 
+    UserDao userDao;
+    User user1;
+    User user2;
+    User user3;
+
+    @BeforeEach
+    void setUp() throws SQLException, ClassNotFoundException{
+        this.userDao = context.getBean("awsUserDao", UserDao.class);
+        this.user1 = new User("1", "AAA", "1111");
+        this.user2 = new User("2", "AAA", "1111");
+        this.user3 = new User("3", "AAA", "1111");
+        System.out.println("before each");
+    }
+
     @Test
     void addAndGet() throws SQLException {
-        User user1 = new User("1", "AAA", "1112");
 
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         userDao.deleteAll();
         assertEquals(0, userDao.getCount());
 
         userDao.add(user1);
         assertEquals(1, userDao.getCount());
-        User user = userDao.findById(user1.getId());
 
+        User user = userDao.findById(user1.getId());
         assertEquals(user1.getName(), user.getName());
         assertEquals(user1.getPassword(), user.getPassword());
 
@@ -38,19 +52,22 @@ class UserDaoTest {
 
     @Test
     void count() throws SQLException{
-        User user1 = new User("1", "AAA", "1112");
-        User user2 = new User("2", "BBB", "1113");
-        User user3 = new User("3", "CCC", "1114");
 
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         userDao.deleteAll();
         assertEquals(0, userDao.getCount());
 
         userDao.add(user1);
         assertEquals(1, userDao.getCount());
-        userDao.add(user1);
+        userDao.add(user2);
         assertEquals(2, userDao.getCount());
-        userDao.add(user1);
+        userDao.add(user3);
         assertEquals(3, userDao.getCount());
+    }
+
+    @Test
+    void findById() {
+        assertThrows(EmptyResultDataAccessException.class, () ->{
+            userDao.findById("5");
+        });
     }
 }
